e5a4a17e31862a20131dc81025f74f53f9d1abd1
@erenJan <erencanerel@gmail.com>
2021-02-06 20:46:13 +0300

    map_improvements

--- lib/map/map.dart
+++ lib/map/map.dart
@@ -33,9 +33,6 @@ class MyMapsState extends State{
   Widget build(BuildContext context) {
     return Scaffold(
         key: scaffoldKey,
-        appBar: AppBar(leading: Icon(Icons.map),
-          backgroundColor: Colors.blue,title:
-          Text("Google Maps With Markers"),),
         body: Stack(
           children: <Widget>[
             _googlemap(context),
@@ -132,7 +129,7 @@ class MyMapsState extends State{
             color:Colors.white,
             elevation: 14.0,
             borderRadius: BorderRadius.circular(24.0),
-            shadowColor: Color(0x802196F3),
+            shadowColor: Colors.white,
             child: Stack(
               children:<Widget>[
                 if(legacy == "UNESCO")
